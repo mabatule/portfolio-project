82012261b4aeae567c3142fb4cdf7c79a83ba0d3
Mauricio Balderrama Ali <74360477+mabatule@users.noreply.github.com>
2024-08-03 18:40:45 -0400

    Add primeNg and first header page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from '../header/header.component';
+
+
+//Prime Modules
+import { TabViewModule } from 'primeng/tabview';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [	
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
